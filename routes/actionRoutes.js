c55db17134ee13a9680c7ea4b1f896fa639529d2
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-22 09:46:41 +0100

    Implemented GET methods

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 
-const Action = require('../data/helpers/actionModel');
+const Actions = require('../data/helpers/actionModel');
 
 routes.use(express.json());
 
-routes.get('/', (req, res) => {
-  console.log('actions working yo!')
+routes.get('/actions', (req, res) => {
+  Actions.get()
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(err => {
+    res.status(500).json({ errorMessage: err})
+  })
 })
 
 module.exports = routes;
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 
-const Project = require('../data/helpers/projectModel');
+const Projects = require('../data/helpers/projectModel');
 
 routes.use(express.json());
 
-routes.get('/s', (req, res) => {
-  console.log('projects are working yo!')
+routes.get('/projects', (req, res) => {
+  Projects.get()
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(err => {
+    res.status(500).json({ errorMessage: err})
+  })
 })
 
 module.exports = routes;
