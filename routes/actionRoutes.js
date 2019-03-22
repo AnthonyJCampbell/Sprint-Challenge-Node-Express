3e29a08e89c25d4318bae9eaebdc9100fabd79bf
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-22 09:53:18 +0100

    Added getById() for both routes

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -7,12 +7,32 @@ routes.use(express.json());
 
 routes.get('/actions', (req, res) => {
   Actions.get()
-  .then(data => {
-    res.status(200).json(data);
-  })
-  .catch(err => {
-    res.status(500).json({ errorMessage: err})
-  })
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: err})
+    })
 })
+
+routes.get('/actions/:id', (req, res) => {
+  const { id } = req.params;
+  Actions.get(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ 
+          message: "The action with the specified ID does not exist." 
+        })
+      } else {
+        res.status(200).json(data);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The action information could not be retrieved."
+      })
+    })
+})
+
 
 module.exports = routes;
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -15,4 +15,25 @@ routes.get('/projects', (req, res) => {
   })
 })
 
+routes.get('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  Projects.get(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ 
+          message: "The Project with the specified ID does not exist." 
+        })
+      } else {
+        res.status(200).json(data);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The Project information could not be retrieved."
+      })
+    })
+})
+
+
+
 module.exports = routes;
